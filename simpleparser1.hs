49e9fab4c105efda5d6a28facc3efc7cbcc6eb65
Andreas Pauley <apauley+github@gmail.com>
2010-08-23 22:58:16 +0200

    Make the parser eat spaces

--- simpleparser1.hs
+++ simpleparser1.hs
@@ -8,9 +8,12 @@ main = do
   putStrLn (readExpr (args !! 0))
 
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
 
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
+
+spaces :: Parser ()
+spaces = skipMany1 space
