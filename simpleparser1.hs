0af53200734a2397a7c839d545a2136223c8ec0e
Andreas Pauley <apauley+github@gmail.com>
2010-08-23 23:09:47 +0200

    Nicer message on success

--- simpleparser1.hs
+++ simpleparser1.hs
@@ -10,7 +10,7 @@ main = do
 readExpr :: String -> String
 readExpr input = case parse (spaces >> symbol) "lisp" input of
   Left err -> "No match: " ++ show err
-  Right val -> "Found value"
+  Right val -> "Found value: " ++ show val
 
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
