b131432ee89642823d206d930c1dbbbb8721db48
Andreas Pauley <apauley+github@gmail.com>
2010-08-24 17:35:34 +0200

    Parse more Lisp goodness(tm)

--- simpleparser1.hs
+++ simpleparser1.hs
@@ -23,6 +23,26 @@ parseExpr :: Parser LispVal
 parseExpr = parseAtom
         <|> parseString
         <|> parseNumber
+        <|> parseQuoted
+        <|> do char '('
+               x <- try parseList <|> parseDottedList
+               char ')'
+               return x
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+  head <- endBy parseExpr spaces
+  tail <- char '.' >> spaces >> parseExpr
+  return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  char '\''
+  x <- parseExpr
+  return $ List [Atom "quote", x]
 
 parseString :: Parser LispVal
 parseString = do char '"'
