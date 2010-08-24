dba5fb51d7b951d6b543f18c47c96b095619d261
Andreas Pauley <apauley+github@gmail.com>
2010-08-24 22:07:44 +0200

    Start to eval expressions

--- simpleparser1.hs
+++ simpleparser1.hs
@@ -4,14 +4,18 @@ import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad
 
 main :: IO ()
-main = do
-  args <- getArgs
-  print (readExpr (args !! 0))
+main = getArgs >>= print . eval . readExpr . head
 
 readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
   Left err -> String $ "No match: " ++ show err
   Right val -> val
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
 
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
