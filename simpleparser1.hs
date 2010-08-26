80fe4d86e0a8e42d0fed215e9b27267f79bd6e15
Andreas Pauley <apauley+github@gmail.com>
2010-08-26 20:43:29 +0200

    boolBinOp

--- simpleparser1.hs
+++ simpleparser1.hs
@@ -42,6 +42,15 @@ numericBinop :: (Integer->Integer->Integer) -> [LispVal] -> ThrowsError LispVal
 numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
 numericBinop op params = mapM unpackNum params >>= return . Number . foldl1 op
 
+boolBinop :: (LispVal->ThrowsError a) -> (a->a->Bool) -> [LispVal] -> ThrowsError LispVal
+boolBinop unpacker op args =
+  if length args /= 2
+  then throwError $ NumArgs 2 args
+  else do
+    left <- unpacker $ args !! 0
+    right <- unpacker $ args !! 1
+    return $ Bool $ left `op` right
+
 unpackNum :: LispVal -> ThrowsError Integer
 unpackNum (Number n) = return n
 unpackNum (String n) = let parsed = reads n in
