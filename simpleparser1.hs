2f0691210037075348f28137c37f4c061baef215
Andreas Pauley <apauley+github@gmail.com>
2010-08-28 13:09:05 +0200

    We have a REPL

--- simpleparser1.hs
+++ simpleparser1.hs
@@ -6,9 +6,16 @@ import Control.Monad.Error
 import IO hiding (try)
 
 main :: IO ()
-main = do
-       args <- getArgs
-       evalAndPrint (args !! 0)
+main =
+  do args <- getArgs
+     case length args of
+       0 -> runRepl
+       1 -> evalAndPrint (args !! 0)
+       otherwise ->
+         putStrLn "Run the program with 0 args for a repl or 1 lisp expression"
+
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Hascheme>>> ") evalAndPrint
 
 readExpr :: String -> ThrowsError LispVal
 readExpr input = case parse parseExpr "lisp" input of
@@ -244,4 +251,11 @@ evalString expr =
 
 evalAndPrint :: String -> IO ()
 evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a->Bool) -> m a -> (a->m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+    then return ()
+    else action result >> until_ pred prompt action
 
