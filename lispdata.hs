5f0b99a62a4bf833753c215463bf16d3df873586
Andreas Pauley <apauley+github@gmail.com>
2010-08-28 19:36:39 +0200

    Move LispError to LispData module

--- lispdata.hs
+++ lispdata.hs
@@ -1,4 +1,5 @@
 module LispData where
+import Text.ParserCombinators.Parsec hiding (spaces)
 
 data LispVal = Atom String
              | List [LispVal]
@@ -18,6 +19,26 @@ showVal (String x) = show x
 showVal (Bool True) = "#t"
 showVal (Bool False) = "#f"
 
+
+data LispError = NumArgs Integer [LispVal]
+               | TypeMismatch String LispVal
+               | Parser ParseError
+               | BadSpecialForm String LispVal
+               | NotFunction String String
+               | UnboundVar String String
+               | Default String
+
+showError :: LispError -> String
+showError (UnboundVar message varname) = message ++ ": " ++ varname
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func) = message ++ ": " ++ show func
+showError (NumArgs expected found) = "Expected " ++ show expected
+                                  ++ " args; found values " ++ unwordsList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
+                                       ++ ", found " ++ show found
+showError (Parser parseErr) = "Parse error at " ++ show parseErr
+
+instance Show LispError where show = showError
+
 unwordsList :: [LispVal] -> String
 unwordsList = unwords . map show
-
--- schemeparser.hs
+++ schemeparser.hs
@@ -174,26 +174,6 @@ parseAtom = do first <- letter <|> symbol
 parseNumber :: Parser LispVal
 parseNumber = liftM (Number . read) $ many1 digit
 
-data LispError = NumArgs Integer [LispVal]
-               | TypeMismatch String LispVal
-               | Parser ParseError
-               | BadSpecialForm String LispVal
-               | NotFunction String String
-               | UnboundVar String String
-               | Default String
-
-showError :: LispError -> String
-showError (UnboundVar message varname) = message ++ ": " ++ varname
-showError (BadSpecialForm message form) = message ++ ": " ++ show form
-showError (NotFunction message func) = message ++ ": " ++ show func
-showError (NumArgs expected found) = "Expected " ++ show expected
-                                  ++ " args; found values " ++ unwordsList found
-showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
-                                       ++ ", found " ++ show found
-showError (Parser parseErr) = "Parse error at " ++ show parseErr
-
-instance Show LispError where show = showError
-
 instance Error LispError where
      noMsg = Default "An error has occurred"
      strMsg = Default
