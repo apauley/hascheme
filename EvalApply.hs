e892635ebb7e466f0c4928227255aacf646cdad6
Andreas Pauley <apauley+github@gmail.com>
2010-09-01 20:54:38 +0200

    Generalise readExpr

--- EvalApply.hs
+++ EvalApply.hs
@@ -5,10 +5,13 @@ import LispData
 import SchemeParser
 import Data.IORef
 
-readExpr :: String -> ThrowsError LispVal
-readExpr input = case parse parseExpr "lisp" input of
+readOrThrow :: Parser a -> String -> ThrowsError a
+readOrThrow parser input = case parse parser "lisp" input of
   Left err -> throwError (Parser err)
   Right val -> return val
+
+readExpr = readOrThrow parseExpr
+readExprList = readOrThrow (endBy parseExpr spaces)
 
 eval :: Env -> LispVal -> IOThrowsError LispVal
 eval env val@(String _) = return val
